19fc0efc3e35dcdc47d12517b6ebfcfbf3173e3d
MI <jiajun.hu@rb.com>
2021-08-30 11:40:09 +0800

    test branch

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from secret import password
 
 def print_hi(name: object) -> object:
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def test_add(a,b):
+    return a+b
 
 
 # Press the green button in the gutter to run the script.
@@ -14,4 +17,6 @@ if __name__ == '__main__':
     print_hi('Karen')
     print_hi('Karen_GG')
 print_hi('Karen_GG_b')
+print(password)
+print(test_add(1,8))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
